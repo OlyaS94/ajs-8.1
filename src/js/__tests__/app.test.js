2dbe2a1a7fe494803ee1ed428bae1aeb9161776c
Olga Smirnova <olya_fcsm@mail.ru>
2022-07-03 17:09:56 +0300

    frs

---
+++ src/js/__tests__/app.test.js
@@ -0,0 +1,34 @@
+import Team from '../app';
+
+test('Should add an archer to the team', () => {
+  const archer = { name: 'Лучник' };
+  const team = new Team();
+  team.add(archer);
+
+  const expected = [archer];
+  const result = team.toArray();
+
+  expect(result).toEqual(expected);
+});
+
+test('Should add an archer and warrior to the team', () => {
+  const archer = { name: 'Лучник' };
+  const warrior = { name: 'Воин' };
+  const team = new Team();
+  team.addAll(archer, warrior, archer);
+
+  const expected = [archer, warrior];
+  const result = team.toArray();
+
+  expect(result).toEqual(expected);
+});
+
+test('Should throw an error as the character already exists', () => {
+  const archer = { name: 'Лучник' };
+  const team = new Team();
+  team.add(archer);
+
+  const expected = Error('Такой персонаж уже есть в команде!');
+
+  expect(() => team.add(archer)).toThrow(expected);
+});
